69faaf3b3657852186def38f6d42c07fcce1ba44
Benjamin Schnitzler <benjamin.schnitzler+git@googlemail.com>
2021-03-23 20:21:27 +0100

    removed rspec again

---
+++ bst/README.md
@@ -0,0 +1,36 @@
+## Binary Search Tree
+
+### Check it out
+
+```sh
+ruby main.rb
+```
+
+### What I wouldn't tell my Psychologist or anyone else ...
+
+Of course it was quite a joy to implement a binary search tree in ruby. However I got stuck with it for quite a while, because I wanted to develope a nice pretty printing method for displaying the tree top to bottom. Another pretty print was already provided by one of the folks of the community (see Credits), but I thought: Nah, I can do better! There I was stuck with my kind of narcisstic perfectionism. I tried it several times and everytime the code got ugly at some point of my journey and I had to withdraw, because of other business, I had to attend to.
+
+Finally I got it right. I'm still pretty proud about this ridiculous archievement; I hope noone will ever find out.
+
+Well I learned a lesson. Nothing to new, I knew it before ... . **Never put all your effort into something which isn't relevant at the end.** Of course You decide yourself, what is relevant and what not. I would definitetly do it again!
+
+On the other hand, the invested time was well put. I learned a lot about ruby, especially Enumerators, which are quite fun.
+
+### TODO
+
+I really wanted to implement an AVLTree subclass, but since I put so much effort in this pettiness of outputting a nicely formatted tree, I decided to leave this open for a future me, maybe ... . Also replacing main.rb with some rspec tests would possibly be a good lesson.
+
+### Credits
+
+Credits go to the [The odin project](https://www.theodinproject.com/) which provides great courses for self teaching ruby, ruby on rails and other web-developement related techniques. I like it, check it out!
+
+### Screenshots
+
+Everyone loves them!
+
+             71             
+     29━━━━━━┻━━━━━━━89     
+ 21━━┻━━━52      74━━┻━━━100
+3┻━28  50┻━54  72┻━79  90━━┛
+
+Now how do You like my tree? Isn't it beautiful. - Gosh! Could someone put some clothes in the mouth of this narcisstic jerk?
--- bst/main.rb
+++ bst/main.rb
@@ -2,57 +2,92 @@
 
 require_relative 'lib/tree'
 
-t = Tree.new([1, 9, 4, 5, 7, 34, 99, 101, 3, 55])
-t.print
-puts "Balanced: #{t.balanced?}"
-t.insert(200)
-t.insert(300)
-t.insert(400)
-t.insert(500)
-t.insert(12)
-t.insert(1)
-t.insert(8)
-t.insert(11)
-t.insert(10)
-t.print
-puts "Balanced: #{t.balanced?}"
+def odin_tests
+  puts "Creating a binary search tree from an array of random numbers ...\n\n"
+  tree = Tree.new(Array.new(15) { rand(1..100) })
+  tree.print
+  puts "\nIs the tree balanced? #{tree.balanced? ? 'Yes.' : 'No.'}\n"
+  puts "\nElements in level order: \n"
+  p tree.level_order
+  puts "\nElements in level pre order: \n"
+  p tree.preorder
+  puts "\nElements in post order: \n"
+  p tree.postorder
+  puts "\nElements in order: \n"
+  p tree.inorder
+  puts "\nTrying to unbalance tree by adding some random numbers greater 100.\n\n"
+  rand(40).times { tree.insert(rand(100..200)) }
+  tree.print
+  puts "\nIs the tree balanced? #{tree.balanced? ? 'Yes.' : 'No.'}\n"
+  puts "\nRebalancing the tree...\n\n"
+  tree.rebalance
+  tree.print
+  puts "\nIs the tree balanced? #{tree.balanced? ? 'Yes.' : 'No.'}\n"
+  puts "\nElements in level order: \n"
+  p tree.level_order
+  puts "\nElements in level pre order: \n"
+  p tree.preorder
+  puts "\nElements in post order: \n"
+  p tree.postorder
+  puts "\nElements in order: \n"
+  p tree.inorder
+end
 
-puts 'Delete 500'
-t.delete(500)
-t.print
-puts 'Delete 300'
-t.delete(300)
-t.print
-puts 'Delete 9'
-t.delete(9)
-t.print
-puts 'Delete 55'
-t.delete(55)
-t.print
-puts 'Delete 55'
-t.delete(55)
-t.print
-puts 'Delete 7'
-t.delete(7)
-t.print
+odin_tests
 
-puts 'Traversel inorder, preorder, postorder, level_order'
-p t.inorder
-p t.preorder
-p t.postorder
-p t.level_order
+def other_tests
+  t = Tree.new([1, 9, 4, 5, 7, 34, 99, 101, 3, 55])
+  t.print
+  puts "Balanced: #{t.balanced?}"
+  t.insert(200)
+  t.insert(300)
+  t.insert(400)
+  t.insert(500)
+  t.insert(12)
+  t.insert(1)
+  t.insert(8)
+  t.insert(11)
+  t.insert(10)
+  t.print
+  puts "Balanced: #{t.balanced?}"
 
-puts 'Find 200'
-p t.find(200)
+  puts 'Delete 500'
+  t.delete(500)
+  t.print
+  puts 'Delete 300'
+  t.delete(300)
+  t.print
+  puts 'Delete 9'
+  t.delete(9)
+  t.print
+  puts 'Delete 55'
+  t.delete(55)
+  t.print
+  puts 'Delete 55'
+  t.delete(55)
+  t.print
+  puts 'Delete 7'
+  t.delete(7)
+  t.print
 
-puts 'Depth 200'
-p t.depth(200)
+  puts 'Traversel inorder, preorder, postorder, level_order'
+  p t.inorder
+  p t.preorder
+  p t.postorder
+  p t.level_order
 
-puts "Height: #{t.height}"
+  puts 'Find 200'
+  p t.find(200)
 
-t.print
-puts "Balanced: #{t.balanced?}"
+  puts 'Depth 200'
+  p t.depth(200)
 
-t.rebalance
-puts "Balanced: #{t.balanced?}"
-t.print
+  puts "Height: #{t.height}"
+
+  t.print
+  puts "Balanced: #{t.balanced?}"
+
+  t.rebalance
+  puts "Balanced: #{t.balanced?}"
+  t.print
+end
